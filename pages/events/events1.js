66bcec223f2d19833419df2b5c49ccc3d0179c76
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2023-01-07 13:14:02 +0600

    events-app

---
+++ pages/events/events1.js
@@ -0,0 +1,9 @@
+const events1 = () => {
+    return (
+        <div>
+            <h1>Aboutus</h1>
+        </div>
+    );
+};
+
+export default events1;
---
+++ pages/events/index.js
@@ -0,0 +1,26 @@
+const Page = () => {
+    return (
+        <div>
+            <h1>Events Page</h1>
+            <div>
+            <a href="">
+          <img/>
+         <h2>Events in London</h2>
+          
+         </a>
+         <a href="">
+          <img/>
+         <h2>Events in Barcelona</h2>
+         
+         </a>
+         <a href="">
+          <img/>
+         <h2>Events in Franscisco</h2>
+          
+         </a> 
+            </div>
+        </div>
+    );
+};
+
+export default Page;
